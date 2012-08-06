e90966083d8d74cddfd0a33cbfea8e5354a8114e
Eduardo <eduardo@ManHack.(none)>
2012-08-05 21:50:01 -0400

    Added dynamic reconfigure for the threshold of the image and tune calibration to get correct postion and orientation

--- uav_camera/src/video_publisher.cpp
+++ uav_camera/src/video_publisher.cpp
@@ -1,4 +1,5 @@
 #include <uav_camera/video_publisher.h>
+#include <iostream>
 
 /* Create a node handle and a video_publisher */
 int main(int argc, char **argv)
@@ -18,10 +19,14 @@ video_publisher::video_publisher(ros::NodeHandle &n): n_ (n), it_ (n_)
 	create_subscribers();
 	capture.open(camera_number);
 	set_camera_calibration();
-	main_loop();
 
-//	main_loop_ = new boost::thread(boost::bind(&video_publisher::main_loop, 
-//												this));
+  dynamic_reconfigure::Server<uav_camera::thresholdConfig> server;
+  dynamic_reconfigure::Server<uav_camera::thresholdConfig>::CallbackType f;
+
+	f = boost::bind(&video_publisher::reconf, this, _1, _2);
+  server.setCallback(f);
+
+	main_loop();
 }
 
 /* Destructor */
@@ -30,25 +35,40 @@ video_publisher::~video_publisher()
 
 }
 
+void video_publisher::reconf(uav_camera::thresholdConfig &config, uint32_t level)
+{
+	threshold_ = config.t;
+	ROS_INFO("New threshold: %d", threshold_);	
+}
+
 void video_publisher::main_loop() 
 {
 	cv::Mat frame;
+//	frame = cv::imread("~/home/eduardo/Desktop/pic_3.jpeg", 1);
+//	ROS_INFO("%d, %d", frame.cols, frame.rows);
 
+//	ros::Rate loop_rate(0.5);
 	ros::Rate loop_rate(50);
 	while(n_.ok()) {
 		/* Grab a new frame */
 		capture >> frame;
-
+	
 		/* Get the undistort image with the camera parameters */
-		cv::Mat undistorted;
-		undistort(frame, undistorted, camera_matrix_, 
-						distorted_coefficients_);
+		cv::Mat undistorted, binary_image;
+		undistort(frame, undistorted, camera_matrix_, distorted_coefficients_);
+
+		binarize(undistorted, binary_image);
+
+//		cv::imwrite("pic.jpeg", undistorted);
+//		ROS_INFO("Image written");
+
 		/* Create message to send the image */
 		cv_bridge::CvImage cvi;
 		cvi.header.stamp = ros::Time::now();
 		cvi.header.frame_id = camera_id;
 		cvi.encoding = "bgr8";
-		cvi.image = undistorted;
+		cvi.image = binary_image;
+//		cvi.image = undistorted;
 //		cvi.image = frame;
 
 		/* Publish the message of the image */
@@ -56,13 +76,17 @@ void video_publisher::main_loop()
 		cvi.toImageMsg(image_msg);
 		image_pub_.publish(image_msg);
 
+		cvi.image = undistorted;
+		cvi.toImageMsg(image_msg);
+		image_pub2_.publish(image_msg);
+
 		/* Create the CameraInfo message */
 		sensor_msgs::CameraInfo info_msg;
 		info_msg.header.stamp = ros::Time::now();
 		info_msg.header.frame_id = camera_id;
 		info_msg.width = resolution_.width;
 		info_msg.height = resolution_.height;
-		info_msg.distortion_model = "";
+		info_msg.distortion_model = "plumb_bob";
 		info_msg.D = cv::vector<double>(ar_params_.d, ar_params_.d + 
 						sizeof(ar_params_.d)/sizeof(*ar_params_.d));
 		info_msg.K = ar_params_.k;
@@ -71,11 +95,38 @@ void video_publisher::main_loop()
 
 		/* Publish the message of the CameraInfo */
 		info_pub_.publish(info_msg);
+		info_msg.header.stamp = ros::Time::now();
+		info_msg.header.frame_id = "raw";
+		info_pub2_.publish(info_msg);
 
+		ros::spinOnce();
 		loop_rate.sleep();
 	}
 }
 
+/* Binary image from RGB8 to RGB8 with given threshold */
+void video_publisher::binarize(cv::Mat &source, cv::Mat &dest)
+{
+	dest = cv::Mat(source.rows, source.cols, source.type());
+	int sum;
+
+	for(int y = 0; y < source.rows; ++y) {
+		uchar *dest_ptr = dest.ptr(y);
+		const uchar *source_ptr = source.ptr(y);
+		for(int x = 0; x < source.cols; ++x) {
+			sum = source_ptr[3*x] + source_ptr[3*x+1] + source_ptr[3*x+2];
+			if(sum < threshold_ * 3) {
+				dest_ptr[3*x] = 0;
+				dest_ptr[3*x+1] = 0;
+				dest_ptr[3*x+2] = 0;
+			} else {
+				dest_ptr[3*x] = 255;
+				dest_ptr[3*x+1] = 255;
+				dest_ptr[3*x+2] = 255;
+			}
+		}
+	}			
+}
 
 /* Get the parameters, if none given, assume defaults */
 void video_publisher::get_params()
@@ -86,10 +137,10 @@ void video_publisher::get_params()
 		camera_number = 0;
 	}
 	if(!n_param.getParam("width", resolution_.width)) {
-		resolution_.width= 320;
+		resolution_.width= 640;
 	}
 	if(!n_param.getParam("height", resolution_.height)) {
-		resolution_.height= 240;
+		resolution_.height= 480;
 	}
 }
 
@@ -104,7 +155,9 @@ void video_publisher::create_publishers()
 	camera_id = cam_id;
 
 	image_pub_ = it_.advertise(cam_image, 1);
+	image_pub2_ = it_.advertise("/raw/image_raw", 1);
 	info_pub_ = n_.advertise<sensor_msgs::CameraInfo>(cam_info, 1);
+	info_pub2_ = n_.advertise<sensor_msgs::CameraInfo>("/raw/camera_info", 1);
 }
 
 /* Create the subscribers to the corresponding topcis */
@@ -123,26 +176,17 @@ void video_publisher::set_camera_calibration()
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, resolution_.width);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, resolution_.height);
 
-	if(resolution_.width == 640) {
+//	if(resolution_.width == 640) {
 	    /* Parameters for camera with fish-eye lens */
-   	 	camera_matrix_ = (cv::Mat_<double>(3,3) << 1408.831918, 0.0, 294.668141,
+/*   	 	camera_matrix_ = (cv::Mat_<double>(3,3) << 1408.831918, 0.0, 294.668141,
 	   	                                           0.0, 1389.801615, 237.959610,
                                          		   0.0, 0.0, 1.0);
 	    distorted_coefficients_ = (cv::Mat_<double>(1,5) << 
 								-3.3458, 10.736563, 0.077535, 0.003666, 0.0);
-
-// 8 distortion factor
-/*        camera_matrix_ = (cv::Mat_<double>(3,3) <<
-438.579386, 0.000000, 327.925578,
-0.000000, 437.194986, 234.398347,
-0.000000, 0.000000, 1.000000
-);
-        distorted_coefficients_ = (cv::Mat_<double>(8,1, CV_64F) <<
-                        //-3.3458, 10.736563, 0.077535, 0.003666, 0.0);
-                0.028721, 0.113968, 0.001450, -0.000785,
-                -0.230102, 0.398666, 0.178484, -0.328546);
 */
-        double temp_d[] = {-0.363638, 0.095521, 0.002198, 0.002716, 0.000000};
+        /* Parameters so ARToolkit think we have a normal   *
+         * camera, and see a rectify image                  */
+/*		double temp_d[] = {-0.363638, 0.095521, 0.002198, 0.002716, 0.000000};
         *ar_params_.d = *temp_d;
         ar_params_.k = { {450.724360, 0.000000, 317.891495,
                           0.000000, 444.956819, 233.815601,
@@ -153,27 +197,105 @@ void video_publisher::set_camera_calibration()
         ar_params_.p = { {450.72436, 0.000000, 317.891495, 0.0,
                           0.000000, 444.956819, 233.815601, 0.0,
                           0.0, 0.0, 1.0, 0.0} };
+*/		
+		//Calibration July 30  640 x 480
+/*		camera_matrix_ = (cv::Mat_<double>(3,3) << 1395.791709, 0.0, 287.558558,
+												   0.0, 1407.777487, 236.280335,
+												   0.0, 0.0, 1.0);
 
-        /* Parameters so ARToolkit think we have a normal   *
-         * camera, and see a rectify image                  */
-/*        double temp_d[] = {-0.363638, 0.095521, 0.002198, 0.002716, 0.000000, 0.0, 0.0, 0.0};
-        *ar_params_.d = *temp_d;
-        ar_params_.k = { {422.790866, 0.000000, 328.485568,
-0.000000, 418.945605, 227.661115,
-0.000000, 0.000000, 1.000000,
-} };
+		distorted_coefficients_ = (cv::Mat_<double>(1,5) <<
+							-3.632066, 8.878626, 0.017618, 0.032021, 0.000000);
+		
+		double temp_d[] = {-0.024614, 0.017706, -0.005636, 0.018934, 0.000000};
+		*ar_params_.d = *temp_d;
+		ar_params_.k = { {428.897061, 0.000000, 320.725215,
+						  0.000000, 424.474553, 242.396429,
+						  0.000000, 0.000000, 1.000000} };
         ar_params_.r = { {1.0, 0.0, 0.0,
-                0.0, 1.0, 0.0,
-                0.0, 0.0, 1.0} };
-        ar_params_.p = { {422.790866, 0.000000, 328.485568, 0.0,
-0.000000, 418.945605, 227.661115, 0.0,
-0.000000, 0.000000, 1.000000, 0.0
-} };
+                          0.0, 1.0, 0.0,
+                          0.0, 0.0, 1.0} };
+		ar_params_.p = { {425.922150, 0.000000, 336.015247, 0.000000,
+						  0.000000, 431.503204, 238.508429, 0.000000,
+ 						  0.000000, 0.000000, 1.000000, 0.000000} };
 */
-} else {
+		//Calibrate aug 1  1280 x 960
+		camera_matrix_ = (cv::Mat_<double>(3,3) << 
+1635.281066, 0.000000, 607.644423,
+0.000000, 1625.623556, 480.528544,
+0.000000, 0.000000, 1.000000);
+
+		distorted_coefficients_ = (cv::Mat_<double>(1,5) <<
+							-1.477884, 1.031956, 0.026532, 0.017903, 0.000000);
+
+		double temp_d[] = {-0.05, 0.0, 0.0, 0.0, 0.0};
+		*ar_params_.d = *temp_d;
+		ar_params_.k = {{ 730.0, 0.0, 640.0,
+						  0.0, 730.0, 480.0,
+						  0.0, 0.0, 1.0 }};
+        ar_params_.r = { {1.0, 0.0, 0.0,
+                          0.0, 1.0, 0.0,
+                          0.0, 0.0, 1.0} };
+		ar_params_.p = {{ 730.0, 0.0, 625.0, 0.0,
+						  0.0, 730.0, 480.0, 0.0,
+						  0.0, 0.0, 1.0, 0.0 }};
+
+/*		double temp_d[] = {-0.024614, 0.017706, -0.005636, 0.018934, 0.000000};
+		*ar_params_.d = *temp_d;
+		ar_params_.k = { {2*428.897061, 0.000000, 2*320.725215,
+						  0.000000, 2*424.474553, 2*242.396429,
+						  0.000000, 0.000000, 1.000000} };
+        ar_params_.r = { {1.0, 0.0, 0.0,
+                          0.0, 1.0, 0.0,
+                          0.0, 0.0, 1.0} };
+		ar_params_.p = { {2*425.922150, 0.000000, 2*336.015247, 0.000000,
+						  0.000000, 2*431.503204, 2*238.508429, 0.000000,
+ 						  0.000000, 0.000000, 1.000000, 0.000000} };
+*/
+/*
+		double temp_d[] = {-1.427884, 1.231956, 0.016532, 0.017903, 0.000000};
+		*ar_params_.d = *temp_d;
+		ar_params_.k = {{1645.281066, 0.000000, 617.644423,
+						0.000000, 1635.623556, 467.528544,
+						0.000000, 0.000000, 1.000000}};
+        ar_params_.r = { {1.0, 0.0, 0.0,
+                          0.0, 1.0, 0.0,
+                          0.0, 0.0, 1.0} };
+		ar_params_.p = {{1645.281066, 0.000000, 617.644423, 0.0,
+						0.000000, 1635.623556, 467.528544, 0.0,
+						0.000000, 0.000000, 1.000000, 0.0}};
+*/	
+/*		CvMat* new_matrix = cvCreateMat(3, 3, CV_32FC1);
+		CvSize size;
+		size.width = 640;
+		size.height = 480;
+		CvSize new_size;	
+		CvMat cam_matrix = camera_matrix_;
+		CvMat dist = distorted_coefficients_;
+	
+		cvGetOptimalNewCameraMatrix(&cam_matrix, &dist, size, 0.0, new_matrix, new_size);	
+	
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				CvScalar scal = cvGet2D(new_matrix, i, j);
+				ROS_INFO("%d: %f", i*4+j, scal.val[0]);
+				ar_params_.k[i*3+j] = scal.val[0];
+				ar_params_.p[i*4+j] = scal.val[0];
+			}
+			ar_params_.p[i*4+3] = 0.0;
+		}					 
+*/
+//		camera_matrix_ = (cv::Mat_<double>(3,3) << 
+//129.590942, 0.0, 280.704895, 0.0, 1228.940918, 238.314178, 0.0, 0.0, 1.0);
+		
+//		cv::Mat amera_matrix_(cam_matrix);
+
+//		double temp_d2[] = {0.0, 0.0, 0.0, 0.0, 0.0};
+//		*ar_params_.d = *temp_d2;
+
+//		} else {
 
 		/* For bottom camera */
-    	camera_matrix_ = (cv::Mat_<double>(3,3) << 214.651930, 0.0, 158.156551,
+/*    	camera_matrix_ = (cv::Mat_<double>(3,3) << 214.651930, 0.0, 158.156551,
 												   0.0, 214.041442, 112.968173,
 												   0.0, 0.0, 1.0);
 
@@ -192,5 +314,8 @@ void video_publisher::set_camera_calibration()
 						  0.000000, 214.999713, 114.332141, 0.0,
 						  0.000000, 0.000000, 1.000000, 0.0} };
 	}
+
+*/
 }
+
 
