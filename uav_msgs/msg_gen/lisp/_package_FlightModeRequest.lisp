33069136100364030bbf54e242d643acbe288803
Venkat <venkat@ManHack.(none)>
2012-10-29 20:28:41 -0400

    Cleaned up some build files that were added by error

--- uav_msgs/msg_gen/lisp/_package_FlightModeRequest.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package uav_msgs-msg)
-(cl:export '(MODE-VAL
-          MODE
-))
--- uav_msgs/msg_gen/lisp/_package_mode_msg.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package uav_msgs-msg)
-(cl:export '(MARKER-VAL
-          MARKER
-          MODE-VAL
-          MODE
-))
